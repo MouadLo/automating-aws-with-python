c20294f89fc12e44ee46ba9bd69b68818a9a1886
mouad.ls <ec2-user@ip-172-31-29-215.us-east-2.compute.internal>
2019-11-30 06:42:48 +0000

    Set up serverless project for videolyzer, and upload test script

--- 03-videolyzer/label_detection.py
+++ 03-videolyzer/label_detection.py
@@ -1,27 +1,29 @@
 # coding: utf-8
 import boto3
+from pathlib import Path
+
 session = boto3.Session()
 s3 = session.resource('s3')
-bucket = s3.create_bucket(
-                Bucket='mouadvideolyzervideos', 
-                                CreateBucketConfiguration={'LocationConstraint': 'us-east-2'}
-                                            )
-from pathlib import path
-from pathlib import Path
-get_ipython().run_line_magic('ls', '/home/ec2-user/environment/automating-aws-with-python/03-videolyzer/*.mp4')
+
+#bucket = s3.create_bucket(Bucket='mouadvideolyzervideos', CreateBucketConfiguration={'LocationConstraint': 'us-east-2'})
+         
+
 pathname = '~/environment/automating-aws-with-python/03-videolyzer/563399566.mp4'
+
 path = Path(pathname).expanduser().resolve()
+
 print(path)
-bucket.upload_file(str(path), str(path.name))
-rekognition-client  = session.client('rekognition')
-rekognition_client  = session.client('rekognition')
-response = rekognition_client.start_label_detection(Video={'S3Object': { 'Bucket': bucket.name, 'Name': path.name}})
+
+#bucket.upload_file(str(path), str(path.name))
+
+
 rekognition_client  = session.client('rekognition', region_name='us-east-2')
-response = rekognition_client.start_label_detection(Video={'S3Object': { 'Bucket': bucket.name, 'Name': path.name}})
-response
-job_id = se['JobId']
-job_id = response['JobId']
-job_id
-result = rekognition_client.get_label_detection(JobId=job_id)
-result
-result['JobStatus']
+#response = rekognition_client.start_label_detection(Video={'S3Object': { 'Bucket': 'mouadvideolyzervideos', 'Name': path.name}})
+#print(response)
+
+#job_id = response['JobId']
+result = rekognition_client.get_label_detection(JobId='867f18ff6fe51f2e1ef5c5ba00ae3751df939885da45a339abd16168f013b333')
+
+print(result)
+
+print(result['JobStatus'])
---
+++ 03-videolyzer/videolyzer/handler.py
@@ -0,0 +1,9 @@
+import json
+
+
+def start_processing_video(event, context):
+    print(event)
+    
+    return
+
+
